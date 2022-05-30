cfd0b1f91995d0e064be06fef44fc4f03207cbd6
annkapopp <88618677+annkapopp@users.noreply.github.com>
2022-05-30 19:27:21 +0200

    add moving avg filter

--- utils.py
+++ utils.py
@@ -1,52 +1,83 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.ndimage import gaussian_filter1d, convolve1d
 
-def plot_acc(data):
+def plot_acc(data, type):
     fig = plt.figure()
+    fig.set_figwidth(20)
     plt.suptitle("Accelerometer")
-    fig.tight_layout()
-    # X
-    plt.subplot(3,1,1)
-    plt.plot(data[0], data[1])
-    plt.xlabel("Time (s)")
-    plt.ylabel("X (m/s^2)")
 
-    # Y
-    plt.subplot(3,1,2)
-    plt.plot(data[0], data[2])
-    plt.xlabel("Time (s)")
-    plt.ylabel("Y (m/s^2)")
+    if type == 'all':
+        # X
+        plt.subplot(3,1,1)
+        plt.plot(data[0], data[1])
+        plt.xlabel("Time (s)")
+        plt.ylabel("X (m/s^2)")
 
-    # Z
-    plt.subplot(3,1,3)
-    plt.plot(data[0], data[3])
-    plt.xlabel("Time (s)")
-    plt.ylabel("Z (m/s^2)")
-    
+        # Y
+        plt.subplot(3,1,2)
+        plt.plot(data[0], data[2])
+        plt.xlabel("Time (s)")
+        plt.ylabel("Y (m/s^2)")
+
+        # Z
+        plt.subplot(3,1,3)
+        plt.plot(data[0], data[3])
+        plt.xlabel("Time (s)")
+        plt.ylabel("Z (m/s^2)")
+
+    elif type == 'norm':
+         # norm
+        plt.plot(data[0], data[1])
+        plt.xlabel("Time (s)")
+        plt.ylabel("|a| (m/s^2)")
+
     plt.show()
     plt.close()
 
-def plot_gyro(data):
+def plot_gyro(data, type):
     fig = plt.figure()
+    fig.set_figwidth(20)
     plt.suptitle("Gyroscope")
-    fig.tight_layout()
+
+    if type == 'all':
     # X
-    plt.subplot(3,1,1)
-    plt.plot(data[0], data[1])
-    plt.xlabel("Time (s)")
-    plt.ylabel("X (rad/s)")
+        plt.subplot(3,1,1)
+        plt.plot(data[0], data[1])
+        plt.xlabel("Time (s)")
+        plt.ylabel("X (rad/s)")
 
-    # Y
-    plt.subplot(3,1,2)
-    plt.plot(data[0], data[2])
-    plt.xlabel("Time (s)")
-    plt.ylabel("Y (rad/s)")
+        # Y
+        plt.subplot(3,1,2)
+        plt.plot(data[0], data[2])
+        plt.xlabel("Time (s)")
+        plt.ylabel("Y (rad/s)")
 
-    # Z
-    plt.subplot(3,1,3)
-    plt.plot(data[0], data[3])
-    plt.xlabel("Time (s)")
-    plt.ylabel("Z (rad/s)")
+        # Z
+        plt.subplot(3,1,3)
+        plt.plot(data[0], data[3])
+        plt.xlabel("Time (s)")
+        plt.ylabel("Z (rad/s)")
+
+    elif type == 'norm':
+        plt.plot(data[0], data[1])
+        plt.xlabel("Time (s)")
+        plt.ylabel("|g| (rad/s)")
 
     plt.show()
     plt.close()
+
+def weighted_moving_average(vol, dim=2, wl=16, sigma=4):
+    '''
+    :param vol: input volume
+    :param dim: target dimension (0-2)
+    :param wl: window length
+    :param sigma: standard deviation of gaussian
+    :return:
+    '''
+    # create 1d dirac impulse
+    dirac = np.zeros(wl)
+    dirac[int(wl // 2)] = 1
+    gauss_weights = gaussian_filter1d(dirac, sigma=sigma)
+    vol = convolve1d(vol, gauss_weights, axis=dim, mode='reflect', origin=0)
+    return vol
